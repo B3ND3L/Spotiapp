a4bebe275d098df971a32ce3d961bee9f05d66fd
B3ND3L <delahais.b@gmail.com>
2016-11-29 11:59:18 +0100

    Api Spotifu + ajout de la recherche et des couvertures Album

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import SearchBar from './components/SearchBar';
+import AlbumList from './components/AlbumList';
+import * as musicApi from './api/musicApi';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = ({
+      albums: [],
+    });
+    this.getAlbums = this.getAlbums.bind(this);
+    this.processAlbums = this.processAlbums.bind(this);
+  }
+
+  getAlbums(artist) {
+    musicApi.getAlbums(artist, this.processAlbums);
+  }
+
+  processAlbums(payload) {
+    this.setState({
+      albums: payload.albums.items,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar getAlbums={this.getAlbums} />
+        <AlbumList albums={this.state.albums} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
